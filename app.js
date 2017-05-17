b30c2c08b1c464c2a95d8bef2bbdf3381e0a6e31
Xchel_Sanchez <xchelsvz@gmail.com>
2017-05-17 16:54:32 -0500

    Basic Configs & Load Esentials Modules

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session       = require('express-session');
+var uuid = require('uuid');
+var multipart = require('connect-multiparty');
+var redis = require('redis');
 
 var routes = require('./routes/index');
 var routes_place = require('./routes/place');
@@ -13,6 +17,10 @@ var routes_user = require('./routes/user');
 var routes_auth = require('./routes/auth');
 
 var app = express();
+//var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var mongoose = require('mongoose');
+var client = redis.createClient();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +29,18 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(multipart());
 app.use(cookieParser());
+app.use(session({
+
+        genid: function (req){ return uuid.v1()},
+        secret: 'q~!!#s4HALA^MADRIDcds4<>>*S3--_-`´ç@',
+        saveUninitialized:  false,
+        resave:       false
+      }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -32,6 +49,17 @@ app.use('/', routes_devotional);
 app.use('/', routes_post);
 app.use('/', routes_user);
 app.use('/', routes_auth);
+
+
+var connStr = 'mongodb://localhost:27017/GenesisAPIDB';
+mongoose.connect(connStr, function(err) {
+    if (err) throw err;
+    console.log('MongoDB al Cientifico!');
+});
+
+client.on('connect', function (){
+    console.log('Nuevo Cliente de Redis al Tiro!');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
