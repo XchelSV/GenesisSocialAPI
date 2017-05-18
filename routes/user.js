0bf280392a229aa51a5a5b3f136a661b63ed907d
Xchel_Sanchez <xchelsvz@gmail.com>
2017-05-17 19:34:14 -0500

    User API Routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var path = require('path');
+var uuid = require('uuid');
+var redis = require('redis');
+var RedisClient = redis.createClient();
+//Models Requires
+var  User = require('../models/user_model');
+
+router.get('/api/user/:token',function (request, response){
+
+	var token = request.params.token;
+	RedisClient.exists(token, function (err, reply){
+		if(reply===1){
+			User.find('',function (err, docs){
+				if (err) throw err;
+				console.log(docs);
+				response.send(docs);	
+			})
+		}
+		else {
+			response.sendStatus(401);
+		}		
+	});
+})
+
+router.get('/api/user/profile/:_userid',function (request,response){
+	var user_id = request.params._userid;
+	User.findById(user_id, function (err,user){
+		if (err) {
+			response.sendStatus(404);
+		}
+		else{
+			response.send(user);
+		}
+	})
+})
+
+router.get('/api/user/img/:_id',function (request,response){
+	var userId = request.params._id;
+	response.sendFile(path.join(__dirname, '../public/img/userPhotos/'+userId+'.jpg'));
+})
 
 module.exports = router;
