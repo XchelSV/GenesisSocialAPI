a5d87c6e09e4659a19a6a41b84a817f9c1030e34
Xchel_Sanchez <xchelsvz@gmail.com>
2017-05-18 16:46:18 -0500

    Place API Routes

--- routes/place.js
+++ routes/place.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var moment = require('moment-timezone');
+var path = require('path');
+var redis = require('redis');
+var RedisClient = redis.createClient();
+
+//Models Requires
+var Place = require('../models/place_model');
+
+router.get('/api/place',function (request,response){
+	Place.find('','',function (err,docs){
+		response.send(docs);
+	})
+})
+
+router.get('/api/users/place/:_placeId',function (request,response){
+	var id = request.params._placeId;
+	Place.findById(id, function (err, place){			
+		User.find({servicePlace: place.formatted_address},function (err, users){
+			response.send(users);
+		})
+	})
+})
 
 module.exports = router;
